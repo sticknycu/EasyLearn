d8730f8b12f1867beeab9e16989e0001d972b223
Nicolae Marius Ghergu <nicolae.ghergu@s.unibuc.ro>
2022-03-23 10:45:00 +0200

    Add files with dependencies

---
+++ src/main/java/ro/nicolaemariusghergu/easylearn/core/EasylearnApplication.java
@@ -0,0 +1,13 @@
+package ro.nicolaemariusghergu.easylearn.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasylearnApplication.class, args);
+    }
+
+}
